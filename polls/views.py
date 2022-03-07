818e0c8c2c12ea04ad7e126195d11b30c68e55d7
ankitpakhale <akp3067@gmail.com>
2022-03-07 22:48:07 +0530

    db commit

--- polls/views.py
+++ polls/views.py
@@ -5,25 +5,25 @@ from .models import *
 # Create your views here.
 
 def index(request):
-    # To save data in Entry table 
-    a = '/home/user5/Documents/ank10/makingQueries/queries1.csv'
-    with open(a, 'r') as f:
-        s = f.readlines()
-        for i in s[1:]:
-            l=i.split(',')
-            qw = Entry.objects.filter(blog = l[0])
-            if not qw:
-                e = Entry()
-                e.blog=Blog.objects.get(id=l[0])
-                e.headline=l[1]
-                e.body_text=l[2]
-                e.pub_date=l[3]
-                e.mod_date=l[4]      
-                e.number_of_comments=l[5]
-                e.number_of_pingbacks=l[6]
-                e.rating=l[7][0]
-                e.save()
-            else:
-                print("data is already there in db!!",l[0])                  
+    # To save data in Entry table in Linux
+    # a = '/home/user5/Documents/ank10/makingQueries/queries1.csv'
+    # with open(a, 'r') as f:
+    #     s = f.readlines()
+    #     for i in s[1:]:
+    #         l=i.split(',')
+    #         qw = Entry.objects.filter(blog = l[0])
+    #         if not qw:
+    #             e = Entry()
+    #             e.blog=Blog.objects.get(id=l[0])
+    #             e.headline=l[1]
+    #             e.body_text=l[2]
+    #             e.pub_date=l[3]
+    #             e.mod_date=l[4]      
+    #             e.number_of_comments=l[5]
+    #             e.number_of_pingbacks=l[6]
+    #             e.rating=l[7][0]
+    #             e.save()
+    #         else:
+    #             print("data is already there in db!!",l[0])                  
     # return render(request, 'index.html')
     return HttpResponse("Hello, world. You're at the polls index.")
