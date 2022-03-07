77e43a83d64001b9857e4a7daec73e834eb621f9
ankitpakhale <akp3067@gmail.com>
2022-03-07 17:21:56 +0530

    data entered using csv

--- djq/settings.py
+++ djq/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djq.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/user5/Documents/ank10/makingQueries/djq/polls/template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
