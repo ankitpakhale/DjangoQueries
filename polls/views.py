225f2585c04a3eaac5cd96548a06373806aa0a20
ankitpakhale <akp3067@gmail.com>
2022-04-07 00:07:40 +0530

    views commit

--- polls/views.py
+++ polls/views.py
@@ -6,8 +6,7 @@ from .models import *
 
 def index(request):
     return HttpResponse("Polls index page")
-
-
+s
 def dataUpload(request):
     # To save data in Entry table in Linux
     a = 'queries1.csv'
